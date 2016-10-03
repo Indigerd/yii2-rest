d1da30c07cdf522649860a746506c0d1d4883641
indigerd <indigerd@templatemonster.me>
2016-10-03 12:59:59 +0300

    added IndexAction with custom search model support

---
+++ src/rest/IndexAction.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace indigerd\rest;
+
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\rest\IndexAction as BaseIndexAction;
+
+class IndexAction extends BaseIndexAction
+{
+    public $modelSearchClass;
+
+    /**
+     * @inheritdoc
+     */
+    protected function prepareDataProvider()
+    {
+        if ($this->prepareDataProvider !== null) {
+            return call_user_func($this->prepareDataProvider, $this);
+        }
+        if ($this->modelSearchClass !== null) {
+            /* @var $modelClass \yii\db\ActiveRecord */
+            $modelSearch = new $this->modelSearchClass;
+            $dataProvider = $modelSearch->search(Yii::$app->getRequest()->getQueryParams());
+        } else {
+            /* @var $modelClass \yii\db\ActiveRecord */
+            $modelClass = $this->modelClass;
+            $dataProvider = new ActiveDataProvider([
+                'query' => $modelClass::find(),
+            ]);
+        }
+        return $dataProvider;
+    }
+}
